f5452e0879a11b066b3f8bc44dfae0bede31a0b6
ABHI ARORA <abhisheetalarora2530@gmail.com>
2025-01-11 23:45:20 +0530

    model predict

--- signLanguage/constant/training_pipeline/__init__.py
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -32,6 +32,13 @@ MODEL_TRAINER_DIR_NAME: str = "model_trainer"
 
 MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov5s.pt"
 
-MODEL_TRAINER_NO_EPOCHS: int = 1
+MODEL_TRAINER_NO_EPOCHS: int = 100
 
 MODEL_TRAINER_BATCH_SIZE: int = 16
+
+
+# """
+# MODEL PUSHER related constant start with MODEL_PUSHER var name
+# """
+# BUCKET_NAME = "sign-lang-abhi"
+# S3_MODEL_NAME = "best.pt"
